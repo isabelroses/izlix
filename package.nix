fa7bc2aa69d7d7ca094b52b7c40ab916fa5c0345
eldritch horrors <pennae@lix.systems>
2025-03-28 23:04:56 +0100

    libstore: use pasta for FODs if available
    This allows using a userspace program, pasta, to handle comms between
    the build sandbox, and the outside world; allowing for full isolation
    including the network namespace, closing the "fixed-output derivation
    talks to the host over an abstract domain socket" hole for good.
    
    Fixes CVE-2025-46416.
    
    Co-Authored-By: Puck Meerburg <puck@puckipedia.com>
    Change-Id: Ifd499b7dbb3784600a6e842fede65fc031ff9f15

--- package.nix
+++ package.nix
@@ -44,6 +44,8 @@
   ninja,
   ncurses,
   openssl,
+  # FIXME: we need passt 2024_12_11.09478d5 or newer, i.e. nixos 25.05 or later
+  passt-lix ? __forDefaults.passt-lix,
   pegtl,
   pkg-config,
   python3,
@@ -115,6 +117,8 @@
     # needs derivation patching to add debuginfo and coroutine library support
     # !! must build this with clang as it is affected by the gcc coroutine bugs
     capnproto-lix = callPackage ./misc/capnproto.nix { inherit stdenv; };
+
+    passt-lix = callPackage ./misc/passt.nix { };
   },
 }:
 
@@ -236,6 +240,7 @@ stdenv.mkDerivation (finalAttrs: {
       # which don't actually get added to PATH. And buildInputs is correct over
       # nativeBuildInputs since this should be a busybox executable on the host.
       "-Dsandbox-shell=${lib.getExe' busybox-sandbox-shell "busybox"}"
+      "-Dpasta-path=${lib.getExe' passt-lix "pasta"}"
     ]
     ++ lib.optional hostPlatform.isStatic "-Denable-embedded-sandbox-shell=true"
     ++ lib.optional (finalAttrs.dontBuild && !lintInsteadOfBuild) "-Denable-build=false"
@@ -345,6 +350,7 @@ stdenv.mkDerivation (finalAttrs: {
     ++ lib.optionals hostPlatform.isLinux [
       libseccomp
       busybox-sandbox-shell
+      passt-lix
     ]
     ++ lib.optionals (
       stdenv.hostPlatform.isDarwin && lib.versionOlder stdenv.hostPlatform.darwinSdkVersion "11.0"
