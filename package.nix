f64eff5e0f0797420a6dfd87fc682a4ea60eef45
Seth Flynn <getchoo@tuta.io>
2025-08-07 19:42:37 -0400

    libexpr: enable parallel marking in boehmgc
    Using parallel marking in GC speeds up evaluation a fair bit
    
    ```
    Benchmark 1: ./lix-main/bin/nix search nixpkgs hello --no-eval-cache
      Time (mean ± σ):     20.740 s ±  0.046 s    [User: 17.583 s, System: 3.062 s]
      Range (min … max):   20.678 s … 20.794 s    10 runs
    
    Benchmark 2: ./lix-parallel-marking/bin/nix search nixpkgs hello --no-eval-cache
      Time (mean ± σ):     15.037 s ±  0.080 s    [User: 19.602 s, System: 3.125 s]
      Range (min … max):   14.960 s … 15.227 s    10 runs
    
    Summary
      ./lix-parallel-marking/bin/nix search nixpkgs hello --no-eval-cache ran
        1.38 ± 0.01 times faster than ./lix-main/bin/nix search nixpkgs hello --no-eval-cache
    ```
    
    Based-on: https://github.com/NixOS/nix/pull/13708
    Co-authored-by: Eelco Dolstra <edolstra@gmail.com>
    Change-Id: Ibc7625f21e0ee7c8ad66203eeb3aca5d83977731

--- package.nix
+++ package.nix
@@ -94,7 +94,15 @@
 
     versionJson = builtins.fromJSON (builtins.readFile ./version.json);
 
-    boehmgc-nix = boehmgc.override { enableLargeConfig = true; };
+    boehmgc-nix = (boehmgc.override { enableLargeConfig = true; }).overrideAttrs {
+      # Increase the initial mark stack size to avoid stack
+      # overflows, since these inhibit parallel marking (see
+      # GC_mark_some()). To check whether the mark stack is too
+      # small, run Nix with GC_PRINT_STATS=1 and look for messages
+      # such as `Mark stack overflow`, `No room to copy back mark
+      # stack`, and `Grew mark stack to ... frames`.
+      NIX_CFLAGS_COMPILE = "-DINITIAL_MARK_STACK_SIZE=1048576";
+    };
 
     editline-lix = editline.overrideAttrs (prev: {
       configureFlags = (prev.configureFlags or [ ]) ++ [
