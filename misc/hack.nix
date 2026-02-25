95d00db70f97ab263b86eb80e775c9497d14e847
isabel <isabel@isabelroses.com>
2026-02-25 17:34:13 +0000

    sourceInfo: 2.94.0-unstable-2026-02-24 -> 2.94.0-unstable-2026-02-25
    Diff: https://github.com/lix-project/lix/compare/6182a5e659c24ce8f85c61a2203691d92563413e...3327a1fcae4cdfc9c5377f0f8cfedb7178c53502

--- misc/hack.nix
+++ misc/hack.nix
@@ -7,14 +7,14 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "lix-source-info";
-  version = "2.94.0-unstable-2026-02-24";
+  version = "2.94.0-unstable-2026-02-25";
 
   # ideally we want to fetch from gitea, but they seem to have their atom file disabled
   src = fetchFromGitHub {
     owner = "lix-project";
     repo = "lix";
-    rev = "6182a5e659c24ce8f85c61a2203691d92563413e";
-    hash = "sha256-Ly8FUEuz33GMGGLoGfMqB1cntxZ4Z5k/M87U1zkfTRc=";
+    rev = "3327a1fcae4cdfc9c5377f0f8cfedb7178c53502";
+    hash = "sha256-j+H9E+ObDc9876KCJMQ16Lm5wFv/TSBmv03oGh57n8k=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
